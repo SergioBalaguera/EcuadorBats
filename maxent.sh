cf994863f221ef2e67396be0f79d1211165b555e
Sergio Balaguera <otto122@hotmail.com>
2018-03-27 21:02:30 -0500

    updated data

--- maxent.sh
+++ maxent.sh
@@ -6,9 +6,9 @@
 #$ -o $JOB_NAME.o$JOB_ID
 #$ -e $JOB_NAME.e$JOB_ID
 #$ -q omni
-#$ -pe mpi 36
+#$ -pe mpi 360
 #$ -P quanah
 
 module load java
 
-java -Xms1G -Xmx4G -jar /home/sbalague/Maxent-master/ArchivedReleases/3.3.3k/maxent.jar nowarnings noprefixes -E "" -E EcuadorBats responsecurves outputformat=raw outputdirectory=/lustre/work/sbalague/ebresults outputfiletype=asc  samplesfile=/home/sbalague/projects/EcuadorBats/bats_double_apostrophe.csv perspeciesresults=true environmentallayers=/home/sbalague/projects/EcuadorBats/ASCII randomseed nowarnings notooltips noaskoverwrite autorun randomtestpoints=0 jackknife=true biasfile=/home/sbalague/projects/EcuadorBats/BiasFile/Bias_File.asc replicates=100 replicatetype=crossvalidate nothreshold nohinge noautofeature nodoclamp maximumiterations=10000 biastype=3 beta_lqp=.95
+java -Xms1G -Xmx4G -jar /home/sbalague/Maxent-master/ArchivedReleases/3.3.3k/maxent.jar nowarnings noprefixes -E "" -E EcuadorBats responsecurves outputformat=raw outputdirectory=/lustre/work/sbalague/ebresults outputfiletype=asc  samplesfile=/home/sbalague/projects/EcuadorBats/Bats_rarefied_points.csv environmentallayers=/home/sbalague/projects/EcuadorBats/ASCII randomseed nowarnings notooltips noaskoverwrite autorun randomtestpoints=0 jackknife=true biasfile=/home/sbalague/projects/EcuadorBats/BiasFile/Bias_File.asc replicates=100 replicatetype=crossvalidate nothreshold nohinge noautofeature nodoclamp maximumiterations=10000 biastype=3 beta_lqp=.95
